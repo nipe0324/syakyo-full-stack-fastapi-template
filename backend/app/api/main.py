596ef28a1d88b1974d225e4ca647cfca27562759
nipe0324 <nipe0324@gmail.com>
2025-06-14 18:24:25 +0900

    feat: utils endpoints

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, users
+from app.api.routes import login, users, utils
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
+api_router.include_router(utils.router)
